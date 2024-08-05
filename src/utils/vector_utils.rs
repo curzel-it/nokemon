93253b68f6f8890bae330cdf0b893664ecc7d189
Federico Curzel <federico@curzel.it>
2024-08-05 19:53:38 +0200

    Fixed ghosting

--- src/utils/vector_utils.rs
+++ src/utils/vector_utils.rs
@@ -13,10 +13,11 @@ pub fn direction_vector(x1: f32, y1: f32, x2: f32, y2: f32) -> Vector2 {
 }
 
 pub fn dumb_direction_vector(x1: f32, y1: f32, x2: f32, y2: f32) -> Vector2 {
-    let up = y2 < y1;
-    let right = x2 > x1;
-    let down = y2 > y1;
-    let left = x2 < x1;
+    let margin = 2.0;
+    let up = y2 - y1 < -margin;
+    let right = x2 - x1 > margin;
+    let down = y2 - y1 > margin;
+    let left = x2 - x1 < -margin;
     return directions_based_direction_vector(up, right, down, left).unwrap_or_default();
 }
 
