0e64b5d3657a13dc5d30dd412eba9d23ba742dbe
Federico Curzel <federico@curzel.it>
2024-08-17 16:29:28 +0200

    Removed junk

--- src/constants.rs
+++ src/constants.rs
@@ -3,7 +3,6 @@ use raylib::math::Rectangle;
 pub const FPS: u32 = 60;
 pub const TILE_VARIATIONS_FPS: f32 = 1.0;
 pub const ANIMATIONS_FPS: f32 = 10.0;
-pub const LOG_GAME_STATE: bool = false; 
 pub const BASE_ENTITY_SPEED: f32 = 30.0;
 pub const ASSETS_PATH: &str = "/Users/curzel/dev/tower-defense/assets";
 pub const NO_PARENT: u32 = 0;
@@ -13,12 +12,8 @@ pub const TILE_SIZE: f32 = 16.0;
 pub const TILE_TEXTURE_SIZE: f32 = 16.0;
 pub const TILE_VARIATIONS_COUNT: u32 = 4;
 pub const COLLISION_THRESHOLD: f32 = TILE_SIZE / 3.0;
-
 pub const HERO_ENTITY_ID: u32 = 69;
 
 // Test Stuff
 
-pub const RECT_ORIGIN_FULL_HD: Rectangle = Rectangle::new(0.0, 0.0, 1920.0, 1080.0);
 pub const RECT_ORIGIN_SQUARE_100: Rectangle = Rectangle::new(0.0, 0.0, 100.0, 100.0);
-pub const RECT_X100_SQUARE_100: Rectangle = Rectangle::new(100.0, 0.0, 100.0, 100.0);
-pub const RECT_ORIGIN_SQUARE_1000: Rectangle = Rectangle::new(0.0, 0.0, 1000.0, 1000.0);
