da709b833e8e32ec794242845fd5bce7cf7aaf35
Federico Curzel <federico@curzel.it>
2024-08-23 07:52:38 +0200

    relative paths

--- src/constants.rs
+++ src/constants.rs
@@ -4,9 +4,9 @@ pub const FPS: u32 = 60;
 pub const TILE_VARIATIONS_FPS: f32 = 1.0;
 pub const ANIMATIONS_FPS: f32 = 10.0;
 pub const BASE_ENTITY_SPEED: f32 = 30.0;
-pub const FONT: &str = "/Users/curzel/dev/tower-defense/fonts/PixelOperator/PixelOperator8.ttf";
-pub const FONT_BOLD: &str = "/Users/curzel/dev/tower-defense/fonts/PixelOperator/PixelOperator8-Bold.ttf";
-pub const ASSETS_PATH: &str = "/Users/curzel/dev/tower-defense/assets";
+pub const FONT: &str = "fonts/PixelOperator/PixelOperator8.ttf";
+pub const FONT_BOLD: &str = "fonts/PixelOperator/PixelOperator8-Bold.ttf";
+pub const ASSETS_PATH: &str = "assets";
 pub const NO_PARENT: u32 = 0;
 pub const INFINITE_LIFESPAN: f32 = -420.0;
 pub const INITIAL_CAMERA_VIEWPORT: Rect = Rect::new(0.0, 0.0, 1000.0, 750.0);
@@ -19,6 +19,8 @@ pub const TILE_VARIATIONS_COUNT: u32 = 4;
 pub const COLLISION_THRESHOLD: f32 = TILE_SIZE / 3.0;
 pub const HERO_ENTITY_ID: u32 = 69;
 pub const INFINITE_STOCK: i32 = -420;
+
+// Sprite Sheets
 pub const SPRITE_SHEET_BLANK: u32 = 1000;
 pub const SPRITE_SHEET_INVENTORY: u32 = 1001;
 pub const SPRITE_SHEET_BIOME_TILES: u32 = 1002;
