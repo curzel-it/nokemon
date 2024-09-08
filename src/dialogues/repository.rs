7de30f1688ec107b4c8146f6d8fdd9feca691a93
Federico Curzel <federico@curzel.it>
2024-09-08 14:12:19 +0200

    Added support for comments in dialogs.json

--- src/dialogues/repository.rs
+++ src/dialogues/repository.rs
@@ -1,16 +1,23 @@
-use std::{collections::HashMap, fs::File, io::BufReader};
-
+use std::{collections::HashMap, fs::File, io::{BufReader, BufRead}};
 use lazy_static::lazy_static;
-
 use crate::constants::DIALOGUES_PATH;
-
 use super::models::Dialogue;
 
 fn load_dialogues_from_json(file_path: &str) -> HashMap<u32, Dialogue> {
-    let file = File::open(file_path).expect("Failed to open dialogues.json file");    
+    let file = File::open(file_path).expect("Failed to open dialogues.json file");
     let reader = BufReader::new(file);
-    let data: Vec<Dialogue> = serde_json::from_reader(reader).expect("Failed to deserialize dialogues from JSON");
-    return data.into_iter().map(|d| (d.id, d)).collect()
+
+    let json_string: String = reader
+        .lines()
+        .filter_map(|line| line.ok()) 
+        .filter(|line| !line.trim_start().starts_with("//"))  
+        .collect::<Vec<String>>()
+        .join("\n");
+
+    let data: Vec<Dialogue> = serde_json::from_str(&json_string)
+        .expect("Failed to deserialize dialogues from JSON");
+
+    data.into_iter().map(|d| (d.id, d)).collect()
 }
 
 lazy_static! {
