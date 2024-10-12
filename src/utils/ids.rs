178dc207c9a008c64433c04772391e82a166d378
Federico Curzel <federico@curzel.it>
2024-10-12 15:50:39 +0200

    Simplified autoinc ids

--- src/utils/ids.rs
+++ src/utils/ids.rs
@@ -1,10 +1,17 @@
-use std::time::{SystemTime, UNIX_EPOCH};
+use lazy_static::lazy_static;
 
+use std::{sync::atomic::{AtomicU32, Ordering}, time::{SystemTime, UNIX_EPOCH}};
+
+lazy_static! {
+    pub static ref NEXT_ID: AtomicU32 = AtomicU32::new(
+        SystemTime::now()
+            .duration_since(UNIX_EPOCH)
+            .expect("Time went backwards")
+            .as_secs()
+            .saturating_sub(1720000000) as u32
+    );
+}
 
 pub fn get_next_id() -> u32 {
-    SystemTime::now()
-        .duration_since(UNIX_EPOCH)
-        .expect("Time went backwards")
-        .as_secs()
-        .saturating_sub(1720000000) as u32
+    NEXT_ID.fetch_add(1, Ordering::Relaxed)
 }
