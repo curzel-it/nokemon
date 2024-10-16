3c005c798f997242b004a0340c846a29e94ddfab
Federico Curzel <federico@curzel.it>
2024-10-16 16:40:13 +0200

    Renamed "Holy Knife" to "Kunai"; Adjusted worlds and second dungeon, cleaned settings/save/inventory

--- src/entities/hero.rs
+++ src/entities/hero.rs
@@ -1,4 +1,4 @@
-use crate::{constants::HERO_HOLY_KNIFE_COOLDOWN, entities::{known_species::SPECIES_HOLY_KNIFE, species::species_by_id}, game_engine::{entity::{Entity, EntityProps}, inventory::{inventory_contains_species, remove_one_of_species_from_inventory}, state_updates::{EngineStateUpdate, WorldStateUpdate}, world::World}, utils::rect::Rect};
+use crate::{constants::HERO_KUNAI_COOLDOWN, entities::{known_species::SPECIES_KUNAI, species::species_by_id}, game_engine::{entity::{Entity, EntityProps}, inventory::{inventory_contains_species, remove_one_of_species_from_inventory}, state_updates::{EngineStateUpdate, WorldStateUpdate}, world::World}, utils::rect::Rect};
 
 impl Entity {
     pub fn setup_hero(&mut self, creative_mode: bool) {
@@ -22,7 +22,7 @@ impl Entity {
         
         world_updates.push(self.cache_props());
         world_updates.push(self.move_camera_update());
-        world_updates.append(&mut self.shoot_holy_knife(world, time_since_last_update));
+        world_updates.append(&mut self.shoot_kunai(world, time_since_last_update));
         world_updates
     }
 
@@ -58,7 +58,7 @@ impl Entity {
         )
     }
     
-    fn shoot_holy_knife(&mut self, world: &World, time_since_last_update: f32) -> Vec<WorldStateUpdate> {
+    fn shoot_kunai(&mut self, world: &World, time_since_last_update: f32) -> Vec<WorldStateUpdate> {
         self.shooting_cooldown_remaining -= time_since_last_update;
         if self.shooting_cooldown_remaining > 0.0 {
             return vec![]
@@ -68,14 +68,14 @@ impl Entity {
             return vec![]
         }
 
-        if !inventory_contains_species(SPECIES_HOLY_KNIFE) {
+        if !inventory_contains_species(SPECIES_KUNAI) {
             return vec![]
         }
 
-        self.shooting_cooldown_remaining = HERO_HOLY_KNIFE_COOLDOWN;
-        remove_one_of_species_from_inventory(SPECIES_HOLY_KNIFE);
+        self.shooting_cooldown_remaining = HERO_KUNAI_COOLDOWN;
+        remove_one_of_species_from_inventory(SPECIES_KUNAI);
 
-        let mut bullet = species_by_id(SPECIES_HOLY_KNIFE).make_entity();
+        let mut bullet = species_by_id(SPECIES_KUNAI).make_entity();
         bullet.direction = world.cached_hero_props.direction;
         let (dx, dy) = bullet.direction.as_col_row_offset();
         bullet.frame = world.cached_hero_props.frame.offset(dx, dy).offset_y(1).with_h(1);
