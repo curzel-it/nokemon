36184ad325516949f9c9c9739c33a7b74918d92c
Federico Curzel <federico@curzel.it>
2024-10-22 14:55:17 +0200

    Added some basic stuff to lib to see if import works

--- src/lib.rs
+++ src/lib.rs
@@ -11,14 +11,40 @@ pub mod ui;
 pub mod utils;
 pub mod worlds;
 
-use serde::{Serialize, Deserialize};
+use game_engine::engine::GameEngine;
+use std::cell::RefCell;
 
-#[no_mangle]
-pub extern "C" fn initialize_game() {
-    // Initialization code
+thread_local! {
+    static ENGINE: RefCell<Option<GameEngine>> = RefCell::new(None);
 }
 
 #[no_mangle]
-pub extern "C" fn update_game() {
-    // Game update logic
+pub extern "C" fn initialize_game(width: i32, height: i32, creative_mode: bool) {
+    ENGINE.with(|engine| {
+        *engine.borrow_mut() = Some(GameEngine::new());
+        if let Some(ref mut eng) = *engine.borrow_mut() {
+            eng.set_creative_mode(creative_mode);
+            eng.start(width, height);
+        }
+    });
+}
+
+#[no_mangle]
+pub extern "C" fn update_game(time_since_last_update: f32) {
+    ENGINE.with(|engine| {
+        if let Some(ref mut eng) = *engine.borrow_mut() {
+            eng.update(time_since_last_update);
+        }
+    });
+}
+
+#[no_mangle]
+pub extern "C" fn is_game_running() -> bool {
+    ENGINE.with(|engine| {
+        if let Some(ref eng) = *engine.borrow() {
+            eng.is_running
+        } else {
+            false
+        }
+    })
 }
